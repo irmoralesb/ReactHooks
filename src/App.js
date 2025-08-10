b16367867211cb039120021095318dffa80689f0
Raul Morales <irmoralesb@hotmail.com>
2025-08-09 22:58:52 -0600

    Additional refactoring for custom hook - for this specific case this may be unneeded.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Layout from "./components/layout/Layout";
-import { ThemeProvider } from './components/context/ThemeContext';
 
 const App = ({ url }) => {
 
   return (
-    <ThemeProvider>
-      <Layout url={url} />
-    </ThemeProvider>
+    <Layout url={url} />
   )
 
 };
